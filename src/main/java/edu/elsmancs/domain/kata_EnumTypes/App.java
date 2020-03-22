b55bfdc1495f353959d1a6531e8fddd3843ea4e3
sebastia98 <sadrover@cifpfbmoll.eu>
2020-03-22 19:28:52 +0100

    Build: Añadir Script principal

--- src/main/java/edu/elsmancs/domain/kata_EnumTypes/App.java
+++ src/main/java/edu/elsmancs/domain/kata_EnumTypes/App.java
@@ -6,8 +6,23 @@ package edu.elsmancs.domain.kata_EnumTypes;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main(String[] args) {
+		 
+		double peso = 1.0; // kg
+		 
+        for(Planeta planeta : Planeta.values()){
+       	 System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
+        
+        System.out.println("\nYour weight only on the terrestrial planets: ");
+        for(Planeta planeta: Planeta.getPlanetasTerrestres()){
+       	 System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
+        
+        System.out.println("\nYour weight only on the gas giant planets: ");
+        for(Planeta planeta: Planeta.getGigantesGaseosos()){
+       	 System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
+	 }
+
 }
