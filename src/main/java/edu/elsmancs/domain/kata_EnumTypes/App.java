9baaaa24a0c38b032c6ded9f660ea24b5f800f7a
sebastia98 <sadrover@cifpfbmoll.eu>
2020-03-22 19:25:09 +0100

    Build: Crear proyecto maven con .gitignore

---
+++ src/main/java/edu/elsmancs/domain/kata_EnumTypes/App.java
@@ -0,0 +1,13 @@
+package edu.elsmancs.domain.kata_EnumTypes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
