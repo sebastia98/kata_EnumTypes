cc998c9b3e3849ed54bbb10b65aa9d0e8e5dd174
sebastia98 <sadrover@cifpfbmoll.eu>
2020-03-23 17:56:28 +0100

    Feat: Método que calcula tu masa dado el peso terrícola

--- src/main/java/edu/elsmancs/domain/kata_EnumTypes/Planeta.java
+++ src/main/java/edu/elsmancs/domain/kata_EnumTypes/Planeta.java
@@ -1,5 +1,6 @@
 package edu.elsmancs.domain.kata_EnumTypes;
 
+
 public enum Planeta {
 	
 	MERCURY(3.303e+23, 2.4397e6), VENUS(4.869e+24, 6.0518e6), EARTH(5.976e+24, 6.37814e6), MARS(6.421e+23, 3.3972e6), JUPITER(1.9e+27,   7.1492e7), SATURN(5.688e+26, 6.0268e7), URANUS(8.686e+25, 2.5559e7), NEPTUNE(1.024e+26, 2.4746e7);
@@ -24,7 +25,11 @@ public enum Planeta {
 	}
 	
 	private double gravity() {
-		return g * (getMasa()/getRadio());
+		return g * (getMasa()/Math.pow(this.getRadio(), 2));
+	}
+	
+	public double masaTerricola(double peso) {
+		return peso / EARTH.gravity();
 	}
 
 	public double pesoSuperficie(double peso) {
